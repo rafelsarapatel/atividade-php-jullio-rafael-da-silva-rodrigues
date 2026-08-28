0df8bf7bd836b12eebecb85779eac26dfc67d221
Jullio rafael rodrigues <lilrafa888@gmail.com>
2026-08-27 21:02:46 -0300

    Add PHP script for multiplication and subtraction

---
+++ questao3.php
@@ -0,0 +1,15 @@
+<?php
+	$n1 = (float) readline("qual seu primeiro numero? ");
+
+     $n2 = (float) readline("qual seu segundo numero ? ");
+
+	 $soma1 = $n1 * $n2;
+
+	  $soma2 = $n1 - $n2;
+
+	 echo("o resultado da multiplição foi: " . $soma1);
+
+	  echo("\n o resultado da subtração foi: " . $soma2);
+
+
+?>
