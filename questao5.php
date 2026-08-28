49140aac5df9f556c3face441c78603bb73dfbb3
Jullio rafael rodrigues <lilrafa888@gmail.com>
2026-08-27 21:03:59 -0300

    Add questao5.php to calculate double and triple values

---
+++ questao5.php
@@ -0,0 +1,17 @@
+<?php
+	$n1 = (float) readline("qual seu primeiro numero para o dobro? ");
+
+     $n2 = (float) readline("qual seu segundo numero para o triplo ? ");
+
+	 $dobro = $n1 * 2;
+
+	  $triplo = $n2 * 3;
+
+     
+	 echo("o resultado do dobro do primeiro numero: " . $dobro);
+
+	  echo("\no resultado do triplo do segundo numero: " . $triplo);
+
+
+
+?>
