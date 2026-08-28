44717212aa43c2f3d48505862ac03ceffe1caad6
Jullio rafael rodrigues <lilrafa888@gmail.com>
2026-08-27 21:03:26 -0300

    Add basic arithmetic operations in questao4.php

---
+++ questao4.php
@@ -0,0 +1,24 @@
+<?php
+	$n1 = (float) readline("qual seu primeiro numero? ");
+
+     $n2 = (float) readline("qual seu segundo numero ? ");
+
+	 $soma0 = $n1 + $n2;
+
+	  $soma2 = $n1 - $n2;
+
+	   $soma1 = $n1 * $n2;
+
+	   $soma3 = $n1 / $n2;
+     
+	 echo("o resultado da soma dos valores foi: " . $soma0);
+
+	  echo("\no resultado da multiplição foi: " . $soma1);
+
+	   echo("\n o resultado da subtração foi: " . $soma2);
+
+	    echo("\n o resultado da divisao foi: " . $soma3);
+
+
+
+?>
