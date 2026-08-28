b7147c66757e3d972377fac7e80eccad830e3044
Jullio rafael rodrigues <lilrafa888@gmail.com>
2026-08-27 21:04:27 -0300

    Add PHP script to calculate arithmetic mean of three numbers

---
+++ questao6.php
@@ -0,0 +1,11 @@
+<?php
+	$n1 = (float) readline("qual seu primeiro numero? ");
+
+     $n2 = (float) readline("qual seu segundo numero? ");
+      
+	  $n3 = (float) readline("qual seu terceiro numero?");
+
+	  $soma = ($n1 + $n2 + $n3) / 3;
+
+	  echo("a media aritimedica desses tres numeros foi: " . $soma);
+      
