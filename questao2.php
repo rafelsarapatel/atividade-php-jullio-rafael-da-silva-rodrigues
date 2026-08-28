97ef0c096f81b0f74313c1880d6d07c76da0f49c
Jullio rafael rodrigues <lilrafa888@gmail.com>
2026-08-27 21:02:17 -0300

    Add PHP script to sum two numbers

---
+++ questao2.php
@@ -0,0 +1,10 @@
+<?php
+	$n1= (float) readline ("qual seu primeiro numero? ");
+    $n2= (float) readline ("qual seu segundo numero? ");
+
+    $soma = $n1 + $n2;
+
+
+	echo("seu numero foi: ". $soma);
+
+?>
